2e0bf506e3db1b98aa355c1eac816485838eceed
ChenyuCastillo <99091295+ChenyuCastillo@users.noreply.github.com>
2022-04-07 17:27:29 +0200

    prueba

--- take_a_bite/change_of_background/cambiarFondo.py
+++ take_a_bite/change_of_background/cambiarFondo.py
@@ -22,6 +22,8 @@ class Ball:
                                   self.color)
 
 
+
+
 class MyGame(arcade.Window):
 
     def __init__(self, width, height, title):
@@ -38,14 +40,20 @@ class MyGame(arcade.Window):
 
 
         arcade.set_background_color(arcade.color.ASH_GREY)
+        arcade.draw_lrtb_rectangle_filled(0, 10, 250, 230, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(SCREEN_WIDTH - 10, SCREEN_WIDTH, 250, 230, arcade.color.BLACK)
 
         # Create our ball
         self.ball = Ball(50, 50, 1, arcade.color.WHITE)
+
+
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
         self.ball.draw()
+        arcade.draw_lrtb_rectangle_filled(0, 10, 250, 230, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(SCREEN_WIDTH - 10, SCREEN_WIDTH, 250, 230, arcade.color.BLACK)
 
 
     def on_mouse_motion(self, x, y, dx, dy):
@@ -58,15 +66,16 @@ class MyGame(arcade.Window):
 
         self.ball.position_y = y
 
-        if self.ball.position_x <= 10:
+        if self.ball.position_x <= 10 and 230 < self.ball.position_y < 250:
             arcade.set_background_color(arcade.color.GREEN)
 
-        if self.ball.position_x >= SCREEN_WIDTH - 10:
+        if self.ball.position_x >= SCREEN_WIDTH - 10 and 230 < self.ball.position_y < 250:
             arcade.set_background_color(arcade.color.BROWN)
 
 
 def main():
     window = MyGame(640, 480, "Drawing Example")
+
     arcade.run()
 
 
